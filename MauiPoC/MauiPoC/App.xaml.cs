f7305232747737f3d408fe4bd164e7b4845a21bd
Jack Wesley Moreira <jackwesley@hotmail.com>
2024-06-12 10:05:10 -0400

    Project creation

---
+++ MauiPoC/MauiPoC/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiPoC
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
